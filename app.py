9c059b5c34736004e6319ea51a8567a02d345429
Salinas5 <agustinsalinas1350@gmail.com>
2025-11-29 23:40:50 -0300

    Creacion de app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from app import create_app
+import logging
+# Ref: https://docs.python.org/3/library/logging.html
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+app = create_app()
+
+#https://flask.palletsprojects.com/en/stable/appcontext/
+app.app_context().push()
