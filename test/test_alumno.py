f3b3b0ef479de19eae2b1d0b3062cc2817b711be
bustosjuliana <168046237+bustosjuliana@users.noreply.github.com>
2025-12-02 18:35:41 -0300

    Creacion de test_alumno

---
+++ test/test_alumno.py
@@ -0,0 +1,70 @@
+import unittest
+import os
+from datetime import date
+from app import create_app, db
+from app.services import AlumnoService
+from test.instancias import nuevoalumno, nuevotipodocumento
+
+class AlumnoTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        os.environ['FLASK_CONTEXT'] = 'testing'
+        self.app = create_app()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+        
+    def test_crear_alumno(self):
+        alumno = nuevoalumno()
+        self.assertIsNotNone(alumno)
+        self.assertIsNotNone(alumno.nombre)
+        self.assertGreaterEqual(alumno.id, 1)
+        self.assertEqual(alumno.nombre, 'Julian')
+        self.assertEqual(alumno.apellido, 'Alvarez')
+        self.assertEqual(alumno.tipo_documento, 'DNI')
+        
+    def test_buscar_alumno_id(self):
+        alumno = nuevoalumno()
+        buscar = AlumnoService.buscar_alumno_id(alumno.alumno_id)
+        self.assertIsNotNone(buscar)
+        self.assertEqual(buscar.nombre, 'Julian')
+        self.assertEqual(buscar.apellido, 'Alvarez')
+        
+    def test_buscar_todos(self):
+        alumno1 = nuevoalumno()
+        tipodoc2 = nuevotipodocumento(dni='44305104', tipo='DNI')
+        alumno2 = nuevoalumno(
+            nombre = 'Julian',
+            apellido = 'Alvarez',
+            sexo = 'M',
+            nroDocumento = '44305104',
+            tipo_documento = tipodoc2 ,
+            nro_legajo = '9975',
+            fechaNacimiento = date(2006,1,5), 
+            fechaIngreso = date(2023,3,1)
+            
+        )
+        
+        alumnos = AlumnoService.buscar_todos()
+        self.assertEqual(len(alumnos),2)
+        
+    def test_actualizar_alumno(self):
+        alumno = nuevoalumno()
+        alumno.nombre = 'Julian Actualizado'
+        alumno.apellido = 'Alvarez Actualizado'
+        alumno_actualizado = AlumnoService.actualizar_alumno(alumno)
+        self.assertEqual(alumno_actualizado.nombre, 'Julian Actualizado')
+        self.assertEqual(alumno_actualizado.apellido, 'Alvarez Actualizado') 
+    
+    def test_borrar_alumno_id(self):
+        alumno = nuevoalumno()
+        resultado = AlumnoService.borrar_alumno_id(alumno.id)
+        self.assertTrue(resultado)
+        busqueda = AlumnoService.buscar_alumno_id(alumno.id)
+        self.assertIsNone(busqueda)
+        
