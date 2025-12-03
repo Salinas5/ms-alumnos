8a30ba65b872c643ce36ce4bb47dcf1df1479fac
Salinas5 <agustinsalinas1350@gmail.com>
2025-12-03 09:14:12 -0300

    arreglo

--- app/mapping/tipo_documento_schema.py
+++ app/mapping/tipo_documento_schema.py
@@ -2,9 +2,10 @@ from marshmallow import fields, Schema, post_load, validate
 from app.models.tipo_documento import TipoDocumento
 
 class TipoDocumentoSchema(Schema):
-    id = fields.Integer(required=True)
-    nombre = fields.String(required=True, validate=validate.Length(max=50))
-    descripcion = fields.String(required=True, validate=validate.Length(max=255))
+    dni = fields.Integer(required=True, validate=validate.Range(min=1000000, max=99999999))
+    libreta_civica = fields.String(required=True, validate=validate.Length(min=1, max=20))
+    libreta_enrolamiento = fields.String(required=True, validate=validate.Length(min=1, max=20))
+    pasaporte = fields.String(required=True, validate=validate.Length(min=1, max=20))
 
     @post_load
     def make_tipo_documento(self, data, **kwargs):
